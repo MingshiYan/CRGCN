e3227824352733bfa0049e02ee2018f27cb8cb3f
MingshiYan <neo.ms.yan@gmail.com>
2022-11-22 22:36:48 +0800

    add source code

---
+++ utils.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : utils.py
+# @Author: yanms
+# @Date  : 2021/10/23 11:26
+# @Desc  :
+import torch
+import torch.nn as nn
+
+
+class BPRLoss(nn.Module):
+    def __init__(self):
+        super(BPRLoss, self).__init__()
+        self.gamma = 1e-10
+
+    def forward(self, p_score, n_score):
+        loss = -torch.log(self.gamma + torch.sigmoid(p_score - n_score))
+        loss = loss.mean()
+
+        return loss
+
+
+class EmbLoss(nn.Module):
+    """ EmbLoss, regularization on embeddings
+
+    """
+
+    def __init__(self, norm=2):
+        super(EmbLoss, self).__init__()
+        self.norm = norm
+
+    def forward(self, *embeddings):
+        emb_loss = torch.zeros(1).to(embeddings[-1].device)
+        for embedding in embeddings:
+            emb_loss += torch.norm(embedding, p=self.norm)
+        emb_loss /= embeddings[-1].shape[0]
+        return emb_loss
+
